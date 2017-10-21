45d8b1e7e57481071309dff66083efb2a3de4667
fz <natureorz@foxmail.com>
2017-10-21 16:08:10 +0800

    Initial commit

---
+++ src/main/java/xyz/fz/dubbo/main/Application.java
@@ -0,0 +1,17 @@
+package xyz.fz.dubbo.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import xyz.fz.dubbo.service.DemoService;
+
+public class Application {
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"application-consumer.xml"});
+        context.start();
+        DemoService demoService = (DemoService) context.getBean("demoService");
+        for (int i=0; i<999; i++) {
+            String hello = demoService.sayHello("world");
+            System.out.println(hello);
+            Thread.sleep(1000L);
+        }
+    }
+}
